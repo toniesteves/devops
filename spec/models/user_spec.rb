1123e5ed3d9573497230ba9de9b8a93fc7afe6b1
Toni Esteves <toni.esteves@gmail.com>
2017-07-16 22:45:01 -0300

    user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: [:model, :user]  do
+  before { @user = build(:user) }
+
+  it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to be_valid }
+
+
 end
