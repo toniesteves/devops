e238d9e8786f19af54cabd6985f8ccf78abd0e2c
Toni Esteves <toni.esteves@gmail.com>
2017-07-19 21:46:32 -0300

    Updating User#info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,22 @@ describe User, type: [:model, :user]  do
 
   context "when user is created" do
 
-    it { expect(subject).to validate_presence_of(:email) }
-    it { expect(subject).to validate_uniqueness_of(:email).case_insensitive }
-    it { expect(subject).to validate_confirmation_of(:password) }
-    it { expect(subject).to allow_value('toni.esteves@gmail.com').for(:email) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to allow_value('toni.esteves@gmail.com').for(:email) }
+    it { is_expected.to validate_uniqueness_of(:auth_token)}
+
+
+    describe '#info' do
+      it 'returns email, created_at and a token' do
+        subject.save!
+        #Mock
+        allow(Devise).to receive(:friendly_token).and_return('abc123xyztoken')
+
+        expect(subject.info).to eq("#{subject.email} - #{subject.created_at} - Token: abc123xyztoken")
+      end
+    end
 
   end
 
